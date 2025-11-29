05c5aecd38d845cd2c506dba73ab93ccdf190ba7
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2025-11-29 20:16:20 +0530

    Program-3

--- Program_3.java
+++ Program_3.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Program_3 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter a number to pritn teh series : ");
+        int a=sc.nextInt();
+
+        int cnt=0;
+        int n=1;
+        while(cnt!=a)
+        {
+            if(n%2==0)
+            {
+                System.out.print(n+" ");
+                cnt++;
+            }
+            n++;
+        }
+    }
 }
